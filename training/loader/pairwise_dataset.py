a77256c0fa867e9bf12d517313776dfc682bb617
jihwan <jihwankim@robotics.snu.ac.kr>
2024-06-26 14:58:43 +0900

    add random pcd option for loader

--- training/loader/pairwise_dataset.py
+++ training/loader/pairwise_dataset.py
@@ -1,12 +1,19 @@
 import os
 import torch
+import numpy as np
 from torch.utils import data
+import pybullet as p
+import open3d as o3d
+o3d.utility.set_verbosity_level(o3d.utility.VerbosityLevel.Error)
 
 from envs import get_env
 
 class Pairwise(torch.utils.data.Dataset):
     def __init__(self, **kwargs):
         self.root = kwargs['root']
+        self.env = kwargs['env']
+        self.random_pcds = kwargs.get('random_pcds', False)
+        self.n_pcd = kwargs.get('n_pcd', 100)
         
         split = kwargs['split']
         
@@ -21,6 +28,20 @@ class Pairwise(torch.utils.data.Dataset):
         for pcd_file in os.listdir(os.path.join(self.root, 'pcds')):
             object_idx = int(pcd_file.split('_')[1].split('.')[0])
             self.pcds[object_idx] = torch.load(os.path.join(self.root, 'pcds', pcd_file)).type(torch.float)
+            
+        self.meshes = []
+        for object_idx in range(n_objects):
+            bID, lID = self.env.env_bullet.idx2id(object_idx)
+            linkinfo = p.getVisualShapeData(bID)[lID+1]
+            mesh = o3d.io.read_triangle_mesh(linkinfo[4])
+            mesh.compute_vertex_normals()
+            self.meshes.append(mesh)
+            
+        self.rand_pcds = []
+        for object_idx in range(n_objects):
+            pcd_object = self.meshes[object_idx].sample_points_uniformly(number_of_points=self.n_pcd*100)
+            pcd_object = torch.tensor(np.array(pcd_object.points), dtype=torch.float).T
+            self.rand_pcds.append(pcd_object)
         
         split_train_val_test = (5/7, 1/7, 1/7)
         num_train_data = int(len(y) * split_train_val_test[0])
@@ -45,8 +66,19 @@ class Pairwise(torch.utils.data.Dataset):
     
     def __getitem__(self, idx):
         pair = self.pair_indices[idx]
-        pcd1 = self.pcds[pair[0]]
-        pcd2 = self.pcds[pair[1]]
+        if self.random_pcds:
+            pcd1 = self._get_random_pcd(pair[0])
+            pcd2 = self._get_random_pcd(pair[1])
+        else:
+            pcd1 = self.pcds[pair[0]]
+            pcd2 = self.pcds[pair[1]]
         SE3 = self.SE3[idx]
         y = self.y[idx]
         return pcd1, pcd2, SE3, y
+    
+    def _get_random_pcd(self, idx):
+        # pcd_object = self.meshes[idx].sample_points_uniformly(number_of_points=self.n_pcd)
+        # pcd_object = torch.tensor(np.array(pcd_object.points), dtype=torch.float).T
+        
+        pcd_object = self.rand_pcds[idx][:, torch.randperm(self.rand_pcds[idx].shape[1])[:self.n_pcd]]
+        return pcd_object
