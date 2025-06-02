03864f8db880c9a102001d3cb67d7093cefad40d
jihwan <kjh6526@robotics.snu.ac.kr>
2025-06-02 17:26:13 +0900

    fix minor error

--- training/model/__init__.py
+++ training/model/__init__.py
@@ -4,9 +4,8 @@ import torch.functional as F
 import sys, os
 
 from training.model.dgcnn import DGCNN
-from training.model.MLP_pcd import MLP_PointCloud
 from training.model.PairwiseNet import PairwiseNet
-from training.model.PcdEncoder import PcdEncoder
+
 
 def get_model(model_cfg, **kwargs):
     name = model_cfg['name']
@@ -14,24 +13,21 @@ def get_model(model_cfg, **kwargs):
     model = model(model_cfg, **kwargs)
     return model
 
+
 def _get_model_instance(name):
     try:
         return {
             'DGCNN': get_DGCNN,
             'PairwiseNet': get_PairwiseNet,
-            'MLP_pcd': get_MLP_pcd,
-            'PCD_autoencoder': get_PCD_autoencoder,
         }[name]
     except:
         raise (f'Model {name} not available.')
+
 
 def get_DGCNN(model_dict, **kwargs):
     model = DGCNN(**model_dict, **kwargs)
     return model
 
-def get_MLP_pcd(model_dict, **kwargs):
-    model = MLP_PointCloud(**model_dict, **kwargs)
-    return model
 
 def get_PairwiseNet(model_dict, **kwargs):
     encoder_cfg = model_dict.encoder
@@ -42,36 +38,23 @@ def get_PairwiseNet(model_dict, **kwargs):
         encoder = encoder(encoder_cfg, **kwargs)
         checkpoint = torch.load(encoder_cfg.root)
         model_state_dict = checkpoint['model_state']
-        encoder_state_dict = {k.replace('encoder.', ''): v for k, v in model_state_dict.items() if k.startswith('encoder.')}
+        encoder_state_dict = {
+            k.replace('encoder.', ''): v
+            for k, v in model_state_dict.items()
+            if k.startswith('encoder.')
+        }
         encoder.load_state_dict(encoder_state_dict, strict=False)
-        
+
         # Set the encoder weights to not be updated during training
         if not encoder_cfg.finetune:
             for param in encoder.parameters():
                 param.requires_grad = False
     else:
         encoder = encoder(encoder_cfg, **kwargs)
-    model = PairwiseNet(encoder=encoder, 
+    model = PairwiseNet(encoder=encoder,
                         hidden_nodes=model_dict.hidden_nodes,
                         activation=model_dict.activation,
                         last_activation=model_dict.last_activation,
                         output_dims=model_dict.output_dims,
                         **kwargs)
     return model
-
-def get_PCD_autoencoder(model_dict, **kwargs):
-    encoder_cfg = model_dict.encoder
-    encoder = _get_model_instance(encoder_cfg.name)
-    encoder = encoder(encoder_cfg, **kwargs)
-
-    decoder_cfg = model_dict.decoder 
-    decoder = _get_model_instance(decoder_cfg.name)
-    decoder = decoder(decoder_cfg, **kwargs)
-
-    model = PcdEncoder(encoder=encoder, 
-                        decoder=decoder,
-                        **kwargs)
-    return model
-
-
-    
